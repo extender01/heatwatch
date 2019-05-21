de701f7de511d8096293fa0a3f7d22ad944e7505
Vit Krajicek <vit@krajicek.org>
2019-05-21 14:43:52 +0200

    promises refactor, http response

--- server.js
+++ server.js
@@ -44,9 +44,9 @@ app.use(express.static('public'));
 //if HEAT class is not found then freeSpots will be NaN,
 //if there are no free places (freeSpots === 0) then recursion via setTimeout is applied, if there is free space then mail is sent and function ends
 
-let userNotified =false
 
-    // RETURNS ARRAY OF PENDING PROMISES, WHICH CHECKS FREE PLACES ON 4 URLS OF URL VARIABLE
+
+    // RETURNS ARRAY OF PENDING PROMISES, WHICH CHECKS FREE PLACES ON 4 URLS OF URL VARIABLE, .then will have array of 4 values (NaN or number)
 const crawlPages = (date, time) => {
    return url.map((singleUrl) => {
         return rp(singleUrl).then((html) => {
@@ -59,7 +59,7 @@ const crawlPages = (date, time) => {
 }
 
 // SEARCHES ARRAY OF RESULTS (IF NO HEAT IS FOUND THEN SOURCE ARRAY IS [NaN,NaN,NaN,NaN], IF IT IS FOUND ONE ELEMENT IS NUMBER), IF HEAT IS FOUND RETURN NUMBER OF FREE SPOTS, IF NOT FOUND RETURN -1
-const deliverFreeSpots = (arrArg) => {
+const getNumberOfFreeSpots = (arrArg) => {
    const result =  arrArg.find((element) => {
         return !isNaN(element)
     });
@@ -73,52 +73,101 @@ const deliverFreeSpots = (arrArg) => {
 
 
 
-const respondToNumOfSpots = (crawlResultArg, dateArg, timeArg, idArg) => {
-    const freeSpots = deliverFreeSpots(crawlResultArg);
-    console.log('probiha then od promise.all, freeSpots je: ', freeSpots)
-    console.log('je id? ', idArg)
 
-   
 
-    if (freeSpots > 0) {
-        console.log(`hura je volnych ${freeSpots} mist`)
-        if(idArg) deleteFromJSON(guard(), idArg);
-    } else if (freeSpots === 0) {
-        console.log(`bohuzel je volnych ${freeSpots} mist`)
-        setTimeout(() => {checkHeat(dateArg, timeArg, idArg)}, 10000)
-    } else if (freeSpots < 0) {
-        console.log('hodina heatu nenalezdena');
-        if(idArg) deleteFromJSON(guard(), idArg);
 
-        
+const httpResponseString = (freeSpotsArg, dateArg, timeArg, emailArg) => {
+    if (freeSpotsArg === 0) {
+        return `Bohužel v žádaném termínu ${dateArg} ${timeArg} není volné místo. Budu to hlídat a pokud se uvolní, napíšu ti na ${emailArg}`
+    } else if (freeSpotsArg > 0) {
+        return `V termínu ${dateArg} ${timeArg} je volné místo (${freeSpotsArg})`
+    } else if (freeSpotsArg < 0) {
+        return `V termínu ${dateArg} ${timeArg} nebyla hodina HEATu nalezena.)`
+    } else {
+        return `neco je spatne`
     }
-    return freeSpots
-}
-
-let numberOfSpots;
-
-const checkHeat = (date, time, id, callback) => {
-    
-    // PROCESSES ALL PROMISES DETECTING NUMBER OF FREE SPOTS AND SUMS THEM TO SINGLE ARRAY and passes them to .then handler
-    Promise.all(crawlPages(date, time))
-    .then((crawlResult) => {respondToNumOfSpots(crawlResult, date, time, id)})
-    .then((freePlaces) => {callback(freePlaces)})
-    
 };
 
-const checkOnRequest = (date, time, email, callback) => {
-    let resultUponPostRequest;
-    const id = uuid()
-    writeToJSON(guard(), {date, time, email, id})
-    checkHeat(date, time, id, callback);
-    return Promise.resolve('jede')
-
+const stopWatching = (id) => {
+    // STOP WATCHING ON DEMAND, create global object with id and state?
 }
 
-//on post request
-// checkOnRequest('21.5', '1800', 'extender01@gmail.com');
 
-// console.log(guard())
+
+const checkHeat = (data, id) => {
+    const {date, time, email} = data //desctructuring
+    
+    // PROCESSES ALL PROMISES DETECTING NUMBER OF FREE SPOTS AND SUMS THEM TO SINGLE ARRAY and passes them to .then handler
+    // crawlResult je napr [NaN,NaN, cislo, NaN]
+    return Promise.all(crawlPages(date, time))
+    .then ((crawlResult) => {
+        const freeSpots = getNumberOfFreeSpots(crawlResult);
+        const currentTime = new Date()
+        const noveId = uuid();
+
+        console.log(currentTime)
+        
+       
+        if (freeSpots === 0) {
+            console.log(`bohuzel je volnych ${freeSpots} mist`)
+
+            // NO ID INCOMING MEANS RUNNING AFTER POST REQUEST FOR 1ST TIME, GENERATE AND SAVE NEW ID AND USE IT ON NEXT RECURSION (WON'T RUN AGAIN)
+            if(!id) {
+                console.log('id neprislo')
+                writeToJSON(guard(), {date, time, email, noveId, stop: false});
+                setTimeout(() => {checkHeat(data, noveId)}, 10000)
+            } else {
+                console.log('id prislo', id)
+                setTimeout(() => {checkHeat(data, id)}, 10000)
+            }
+        }
+        else if (freeSpots > 0) {
+
+            console.log(`hura je volnych ${freeSpots} mist, POSLEM ZPRAVU NA ${email}`)
+            // ID EXISTS MEANS ITS NOT RUNNING FOR 1ST TIME SO DELETE FROM JSON AND SEND MESSAGE (FOR FIRST TIME NO NEED TO SEND MESSAGE)
+            if(id) {
+                deleteFromJSON(guard(), id);
+                mail.sendEmail(email, date, time)
+            } 
+
+        } else if (freeSpots < 0) {
+            
+            console.log('hodina heatu nenalezdena');
+            if(id) deleteFromJSON(guard(), id);
+
+        } else {
+
+            console.log( 'Něco se pokazilo při zjišťování počtu volných míst')
+            throw 'Něco se pokazilo při zjišťování počtu volných míst'
+        }
+
+        
+
+        // RETURNING FREE SPOTS FOR THEN HANDLER IN HTTP RESPONSE OF EXPRESS ROUTE
+        return freeSpots
+    })
+
+
+       
+
+};
+
+const checkOnRequest = (data) => {
+   
+    // const id = uuid();
+    // writeToJSON(guard(), {date, time, email, id});
+    
+    return checkHeat(data, undefined) //undefined beacuse no id for 1st time
+    .then((freeSpots) => {
+        
+        const responseMessage = httpResponseString(freeSpots, data.date, data.time, data.email)
+        console.log('bezi to furt?')
+        return responseMessage
+    });
+    
+
+};
+
 
 const checkOnRestart = () => {
     const loadedGuards = guard();
@@ -136,160 +185,6 @@ const checkOnRestart = () => {
 
 
 
-// console.log(checkForSpots('21.5', '1700'));
-//VYRESIT ZE USERNOTIFIED SE PREPISE NA TRUE PRI PRVNIM POST REQUESTU A PAK UZ ZUSTANE PORAD TRUE TAKZE U DALSICH REQUESTU SE NEPROVEDE CALLBACK RES.SEND !!!!!!!!!!!!!
-
-// const promiseHtml = async () => {
-
-// }
-
-// async function f() {
-//     return 'ahoj';
-// }
-// f().then((vysledek) => {console.log(vysledek)}); // 1
-
-// const free = (dateArg, timeArg) => {
-//     let pagesChecked = 0
-//     let freeSpots
-//     const reduced = url.reduce( async  (accumulator, currentWebPage) => {
-//          await rp(currentWebPage).then((html) => {
-//             console.log('provadim reduce', pagesChecked)
-//             freeSpots = parseInt($(`td:contains(${dateArg})`, html).siblings('td').children(`[href*='time=${timeArg}']`).children('div.free').text(),10) //get number of free spots
-//             if(pagesChecked > 2) {
-//                 accumulator = -1
-//                 return accumulator
-//             }
-//             if(!isNaN(freeSpots)) {
-
-//             }
-
-            
-            
-//             if(isNaN(freeSpots)) {
-//                 ++pagesChecked
-//             } 
-//         })
-//         // return 'baf'
-
-//     },[])
-//     console.log(reduced)
-// };
-
-// // console.log(free('20.5', '1800'));
-// free('20.5', '1800')
-
-//////////////////////////////////////////////////////
-
-// function test(url) {
-//     return rp(url)
-// }
-
-// let final = []
-
-// function delej(arr) {
-//     return arr.reduce((promise, item) => {
-//         return promise.then((result) => {
-//             console.log(`probiha promisa ${item}`);
-//             return test(item).then((result2) => {
-//                 console.log('probiha push', item)
-//                 final.push(result2)
-//             })
-//         })
-//     }, Promise.resolve())
-// }
-
-// delej(url).then(() => {console.log('tohle je vysledek')})
-
-    ///////////////////////////////////////////////////////////////
-// function dupej(arr) {
-//     return arr.reduce((promise, item) => {
-//         return promise.then((res) => {
-
-//         })
-//     })
-// }
- 
-
-const checkHeatPages = (adresa, datum, cas, callback) => {
-    let pagesChecked = 0;
-    let saveRecord
-    url.forEach((website) => {
-        rp(website).then((html) => {
-            let freeSpots = parseInt($(`td:contains(${datum})`, html).siblings('td').children(`[href*='time=${cas}']`).children('div.free').text(),10) //get number of free spots
-            console.log('freespots', freeSpots)
-            if(!isNaN(freeSpots)) {     //freeSpots is number, HEAT class found
-                if(freeSpots === 0) {   //freeSpots is 0 -> no free spot, aplly recursion by setTimeout
-                    if(!userNotified) {
-                        callback(`bohuzel, v terminu ${datum} ${cas} neni volne misto, budu ti to hlidat a kdyztak poslu mail na adresu ${adresa} `);
-                        fs.writeFileSync('guard.json', JSON.stringify(uuid()))
-                        console.log(guard)
-                        userNotified = true
-                    }
-                    setTimeout(() => {
-                        checkHeatPages(adresa, datum, cas, callback)
-                    }, timeout) 
-                   
-                    let currentTime = new Date().toLocaleTimeString();
-                    console.log(`${currentTime} bohuzel, v terminu ${datum} ${cas} neni volne misto: `, freeSpots)
-                } else {      //freeSpots is number but not 0 -> free spot found!
-                   
-                    let currentTime = new Date().toLocaleTimeString();
-
-                    console.log(`${currentTime} hura, naslo se ${freeSpots} v terminu ${datum} ${cas} `)
-
-                    if(!userNotified){
-                        callback(`hodina HEATu v terminu ${datum} ${cas} MA ${freeSpots} VOLNE MISTA, zkontroluj to na heatopava.cz`)
-                        userNotified = true
-                    }
-
-                    state = 'uspech';
-                    mail.sendEmail(adresa, datum, cas)
-
-                    return state;
-                   
-                   
-                }
-    
-    
-            } else {    //freeSpots is NaN -> HEAT class not found on webpage, if that happens 4 times
-                pagesChecked++;
-
-                // console.log('hodina heatu nenalezena, pocitadlo je: ', pagesChecked);
-                
-                if(pagesChecked === 4) {
-                    let currentTime = new Date().toLocaleTimeString();
-                    console.log(currentTime);
-                    console.log('zkontrolovany vsechny 4 strany, hodina heatu nenalezena' )
-                    callback(`hodina HEATu v terminu ${datum} ${cas} nenalezena, zkontroluj termin na heatopava.cz`)
-
-                    pagesChecked = 0;
-
-                }
-    
-            }
-        })
-        
-        .catch((err) => {
-            console.log(err);
-
-            
-        });
-    });
-
-    
-
-};
-
-
-// guard.forEach((element) => {
-//     checkHeatPages(element.email, element.date, element.time, (message) => {console.log('z JSONu: ', message)})
-// })
-
-
-
-
-
-
 
 app.get('/', function (req, res) {
     res.render('index');
@@ -298,14 +193,21 @@ app.get('/', function (req, res) {
 
 app.post('/send', (req,res) => {
     console.log('muzeme zacinati zaklinati');
-    // checkHeatPages(req.body.email, req.body.date, req.body.time, (message) => {
-    //     // console.log('callback po probehnuti then')
-    //     res.send(message);
-    // });
-    checkOnRequest(req.body.date, req.body.time, req.body.email, (number) => {res.send(`volnych mist je ${number}`)})
 
+    // checkOnRequest(req.body.date, req.body.time, req.body.email)
+    checkOnRequest({date: req.body.date, time: req.body.time, email: req.body.email})
 
+    .then((message) => {
+        res.send(message)
+    })
+    .catch((err) => {
+        res.send(err)
+    })
 });
+
+app.get('/rusim/:id', (req, res) => {
+    res.send(req.params.id)
+})
 
 
 
